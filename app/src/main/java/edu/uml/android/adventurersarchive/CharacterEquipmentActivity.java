e2340f8fca89bd1c18c7aa37eabf72a97fd56197
therealEthanGoldman <ethan_goldman@student.uml.edu>
2016-11-15 20:46:12 -0500

    Added bare framework
    Still do not know how to do a proper layout, but the means for total
    weight, a toggleble equip, and coins are there.

--- app/src/main/java/edu/uml/android/adventurersarchive/CharacterEquipmentActivity.java
+++ app/src/main/java/edu/uml/android/adventurersarchive/CharacterEquipmentActivity.java
@@ -11,6 +11,50 @@ import edu.uml.android.adventurersarchive.character.CharacterInfo;
  */
 public class CharacterEquipmentActivity extends AppCompatActivity {
     private CharacterInfo myCharacter;
+    int totalweight;
+
+    public class Equipment
+    {
+        double wt;
+        String name;
+        String description;
+        boolean isEquiptable, isEquiped;
+        int quanity;
+        boolean attunable;
+        coins value;
+
+        Equipment(){
+            wt = 0;
+            name = "Blank Slate";
+            description = "This item is a default";
+            isEquiptable = false;
+            isEquiped = false;
+            quanity = 0;
+            attunable = false;
+            value = new coins( );
+        }
+
+        Equipment( double weight, String newname, String Newdesc, boolean equiptable, int howmuch, boolean attuned, coins kaching)
+        {
+            wt = weight;
+            totalweight += weight;
+            name = newname;
+            description = Newdesc;
+            isEquiptable = equiptable;
+            isEquiped = false;
+            quanity = howmuch;
+            attunable = attuned;
+            value = kaching;
+        }
+
+        void toggleEquiped( boolean val){
+            if ( isEquiptable == true)
+                isEquiped = val;
+        }
+
+
+    }
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -18,7 +62,117 @@ public class CharacterEquipmentActivity extends AppCompatActivity {
         setContentView(R.layout.activity_character_equipment);
 
         Intent intent = getIntent();
+        myCharacter = intent.getParcelableExtra("character");
 
-        myCharacter = (CharacterInfo) intent.getParcelableExtra("character");
+
+
+    }
+
+
+    public class coins{
+        int cp, sp, ep, gp, pp;
+
+        coins()
+        {
+            cp = 0;
+            sp = 0;
+            ep = 0;
+            gp = 0;
+            pp = 0;
+        }
+
+        coins( int c, int s, int e, int g, int p)
+        {
+            cp = c;
+            sp = s;
+            ep = e;
+            gp = g;
+            pp = p;
+
+            convert();
+
+        }
+        void convert(){
+            while (cp > 10)
+            {
+                cp = cp - 10;
+                sp++;
+            }
+            while (sp > 5)
+            {
+                sp = sp - 5;
+                ep++;
+            }
+            while (ep > 2)
+            {
+                ep = ep - 2;
+                gp++;
+            }
+            while (gp > 10)
+            {
+                gp = gp - 10;
+                pp++;
+            }
+        }
+        void set( coins newv)
+        {
+            cp = newv.cp;
+            sp = newv.sp;
+            ep = newv.ep;
+            gp = newv.gp;
+            pp = newv.pp;
+
+            convert();
+
+        }
+        void add(coins loot)
+        {
+            cp = cp + loot.cp;
+            sp = sp + loot.sp;
+            ep = ep + loot.ep;
+            gp = gp + loot.gp;
+            pp = pp + loot.pp;
+            convert();
+        }
+
+        boolean take(coins cost)
+        {
+            cp = cp - cost.cp;
+
+            sp = sp - cost.sp;
+            ep = ep - cost.ep;
+            gp = gp - cost.gp;
+            pp = pp - cost.pp;
+
+            return negconvert();
+        }
+
+        boolean negconvert()
+        {
+            while (cp < 0)
+            {
+                sp--;
+                while (sp <0 )
+                {
+                    ep--;
+                    while (ep < 0)
+                    {
+                        gp--;
+                        while(gp < 0)
+                        {
+                            pp--;
+                            if (pp < 0)
+                                return false;
+                            gp = gp + 10;
+                        }
+                        ep = ep + 2;
+                    }
+                    sp = sp + 5;
+                }
+                cp = cp + 10;
+            }
+            return true;
+        }
     }
 }
+
