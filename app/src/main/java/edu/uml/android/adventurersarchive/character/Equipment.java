2eaef3d8606243e04a73e08cf5907b85010c061a
therealEthanGoldman <ethan_goldman@student.uml.edu>
2016-11-27 21:10:09 -0500

    started adding equipment list

--- app/src/main/java/edu/uml/android/adventurersarchive/character/Equipment.java
+++ app/src/main/java/edu/uml/android/adventurersarchive/character/Equipment.java
@@ -1,30 +1,33 @@
 package edu.uml.android.adventurersarchive.character;
 
-import edu.uml.android.adventurersarchive.CharacterEquipmentActivity;
-
 /**
  * Created by Golg on 11/16/2016.
  */
 
 public class Equipment
 {
-    double wt;
-    String name;
-    String description;
-    boolean isEquiptable, isEquiped;
-    int quanity;
-    boolean attunable;
-    Coins value;
+    public int id;
+    public double wt;
+    public String name;
+    public String description;
+    public boolean isEquiptable, isEquipped;
+    public int quanity;
+    public boolean attuneable;
+    public Coins value;
+    private static int lastUsedId = 0;
+
 
     Equipment(){
         wt = 0;
         name = "Blank Slate";
         description = "This item is a default";
         isEquiptable = false;
-        isEquiped = false;
+        isEquipped = false;
         quanity = 0;
-        attunable = false;
+        attuneable = false;
         value = new Coins( );
+        lastUsedId++;
+        id = lastUsedId;
     }
 
     Equipment( double weight, String newname, String Newdesc, boolean equiptable, int howmuch, boolean attuned, Coins kaching)
@@ -33,15 +36,17 @@ public class Equipment
         name = newname;
         description = Newdesc;
         isEquiptable = equiptable;
-        isEquiped = false;
+        isEquipped = false;
         quanity = howmuch;
-        attunable = attuned;
+        attuneable = attuned;
         value = kaching;
+        lastUsedId++;
+        id = lastUsedId;
     }
 
     void toggleEquiped( boolean val){
         if ( isEquiptable == true)
-            isEquiped = val;
+            isEquipped = val;
     }
 
 
